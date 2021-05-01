17464c2bafe1a3486d11fb81addf30c5a33d787c
Solo-fl <francesco.l@solomotorcontrollers.com>
2021-05-01 20:53:26 +0200

    chartja3

--- assets/js/monitor.js
+++ assets/js/monitor.js
@@ -125,274 +125,220 @@ function onRefresh(chart) {
 
 var color = Chart.helpers.color;
 var config = {
-	type: 'bar',
+	type: 'line',
 	data: {
 		labels: [],
 		datasets: [{
             label: 'VA [V]',
             backgroundColor: window.chartColors.blue,
             borderColor: window.chartColors.blue,
-            yAxisID: 'y-axis-V',
+            yAxisID: 'y_axis_V',
             commandValue: '82',
             commandConversion: 'SFXT',
             hidden: true,
 
 			type: 'line',
-			fill: false,
-            cubicInterpolationMode: 'monotone',
-            borderWidth: 1,
-            pointRadius: 0,
-            lineTension: 0,
             data: []
 		}, {
             label: 'VB [V]',
             backgroundColor: window.chartColors.green,
             borderColor: window.chartColors.green,
-            yAxisID: 'y-axis-V',
+            yAxisID: 'y_axis_V',
             commandValue: '83',
             commandConversion: 'SFXT',
             hidden: true,
 
 			type: 'line',
-			fill: false,
-            cubicInterpolationMode: 'monotone',
-            borderWidth: 1,
-            pointRadius: 0,
-            lineTension: 0,
 			data: []
         }, {
             label: 'VC [V]',
             backgroundColor: window.chartColors.black,
             borderColor: window.chartColors.black,
-            yAxisID: 'y-axis-V',
+            yAxisID: 'y_axis_V',
             commandValue: 'SUM',
             hidden: true,
 
 			type: 'line',
-			fill: false,
-            cubicInterpolationMode: 'monotone',
-            borderWidth: 1,
-            pointRadius: 0,
-            lineTension: 0,
 			data: []
 		}, {
             label: 'IA [A]',
             borderColor: window.chartColors.navy,
             backgroundColor: window.chartColors.navy,
-            yAxisID: 'y-axis-A',
+            yAxisID: 'y_axis_A',
             commandValue: '84',
             commandConversion: 'SFXT',
             hidden: true,
 
 			type: 'line',
-			fill: false,
-            cubicInterpolationMode: 'monotone',
-            borderWidth: 1,
-            pointRadius: 0,
-            lineTension: 0,
 			data: []
 		}, {
             label: 'IB [A]',
             borderColor: window.chartColors.teal,
             backgroundColor: window.chartColors.teal,
-            yAxisID: 'y-axis-A',
+            yAxisID: 'y_axis_A',
             commandValue: '85',
             commandConversion: 'SFXT',
             hidden: true,
 
 			type: 'line',
-			fill: false,
-            cubicInterpolationMode: 'monotone',
-            borderWidth: 1,
-            pointRadius: 0,
-            lineTension: 0,
 			data: []
         }, {
             label: 'IC [A]',
             borderColor: window.chartColors.yellow,
             backgroundColor: window.chartColors.yellow,
-            yAxisID: 'y-axis-A',
+            yAxisID: 'y_axis_A',
             commandValue: 'SUM',
             hidden: true,
 
 			type: 'line',
-			fill: false,
-            cubicInterpolationMode: 'monotone',
-            borderWidth: 1,
-            pointRadius: 0,
-            lineTension: 0,
 			data: []
 		},{
             label: 'Vsupply [V]',
             borderColor: window.chartColors.acqua,
             backgroundColor: window.chartColors.acqua,
-            yAxisID: 'y-axis-V',
+            yAxisID: 'y_axis_V',
             commandValue: '86',
             commandConversion: 'SFXT',
 
 			type: 'line',
-			fill: false,
-            cubicInterpolationMode: 'monotone',
-            borderWidth: 1,
-            pointRadius: 0,
-            lineTension: 0,
 			data: []
 		}, {
             label: 'IM_DC motor [A]',
             borderColor: window.chartColors.orange,
             backgroundColor: window.chartColors.orange,
-            yAxisID: 'y-axis-A',
+            yAxisID: 'y_axis_A',
             commandValue: '87',
             commandConversion: 'SFXT', 
             hidden: true,           
 
 			type: 'line',
-			fill: false,
-            cubicInterpolationMode: 'monotone',
-            borderWidth: 1,
-            pointRadius: 0,
-            lineTension: 0,
 			data: []
 		}, {
             label: 'VM_DC motor [V]',
             borderColor: window.chartColors.lime,
             backgroundColor: window.chartColors.lime,
-            yAxisID: 'y-axis-V',
+            yAxisID: 'y_axis_V',
             commandValue: '88',
             commandConversion: 'SFXT',
             hidden: true,
 
 			type: 'line',
-			fill: false,
-            cubicInterpolationMode: 'monotone',
-            borderWidth: 1,
-            pointRadius: 0,
-            lineTension: 0,
 			data: []
 		}, {
             label: 'Iq _ Torque [A]',
             borderColor: window.chartColors.purple,
             backgroundColor: window.chartColors.purple,
-            yAxisID: 'y-axis-A',
+            yAxisID: 'y_axis_A',
             commandValue: '8D',
             commandConversion: 'SFXT',
             
 
 			type: 'line',
-			fill: false,
-            cubicInterpolationMode: 'monotone',
-            borderWidth: 1,
-            pointRadius: 0,
-            lineTension: 0,
 			data: []
 		}, {
             label: 'Id _ Magnetizing Current [A]',
             borderColor: window.chartColors.olive,
             backgroundColor: window.chartColors.olive,
-            yAxisID: 'y-axis-A',
+            yAxisID: 'y_axis_A',
             commandValue: '8E',
             commandConversion: 'SFXT',
             hidden: true,
 
 			type: 'line',
-			fill: false,
-            cubicInterpolationMode: 'monotone',
-            borderWidth: 1,
-            pointRadius: 0,
-            lineTension: 0,
 			data: []
 		}, {
             label: 'Speed [RPM]',
             borderColor: window.chartColors.red,
             backgroundColor: window.chartColors.red,
-            yAxisID: 'y-axis-RPM',
+            yAxisID: 'y_axis_RPM',
             commandValue: '96',
             commandConversion: 'UINT32',
             
 
 			type: 'line',
-			fill: false,
-            cubicInterpolationMode: 'monotone',
-            borderWidth: 1,
-            pointRadius: 0,
-            lineTension: 0,
 			data: []
 		}]
 	},
 	options: {
-      
-        legend: {
-            labels: {
-              padding: 20,
-              fontSize:14,
-            }
-          },
-    
-        animation: {
-            duration: 0
-        },
-		scales: {
-			xAxes: [{
-                display: true,
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Samples',
-                    fontSize:14,
+        animation: false,
+        spanGaps: true,
+        pointRadius : 0,
+        borderWidth: 1,
+        cubicInterpolationMode: 'monotone',
+
+        plugins :{
+            tooltip: {
+                //enabled : false,
+               position : 'nearest',
+		    },
+            legend: {
+                labels: {
+                  padding: 20,
+                  font:{
+                    size:14,
+                  },
                 }
-			}],
-			yAxes: [{
+              },
+        },
+
+		scales: {
+			x: {
+                display: true,
+                title: {
+                    display: true,
+                    text: 'Samples',
+                    font:{
+                        size:14,
+                    }
+                }
+			},
+			y_axis_V: {
                 type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
                 display: true,
                 position: 'left',
-                id: 'y-axis-V',
-                scaleLabel: {
+                title: {
                     display: true,
-                    labelString: 'Volts',
-                    fontSize:14,
-
+                    text: 'Volts',
+                    font:{
+                        size:14,
+                    },
                 },
                  // grid line settings
-                gridLines: {
+                grid: {
                     drawOnChartArea: false, // only want the grid lines for one axis to show up
                 },
-            }, {
+            }, 
+            y_axis_A:{
                 type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
                 display: true,
                 position: 'left',
-                id: 'y-axis-A',
-                scaleLabel: {
+                title: {
                     display: true,
-                    labelString: 'Amps',
-                    fontSize:14,
+                    text: 'Amps',
+                    font : {
+                        size:14,
+                    }
                 },
 
                 // grid line settings
-                gridLines: {
+                grid: {
                     drawOnChartArea: false, // only want the grid lines for one axis to show up
                 },
             },
 
-            {
+            y_axis_RPM:{
                 type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
                 display: true,
                 position: 'right',
-                id: 'y-axis-RPM',
-                labelString: '[RPM]',
-                scaleLabel: {
+                title: {
                     display: true,
-                    labelString: 'RPM',
-                    fontSize:14,
+                    text: 'RPM',
+                    font:{
+                        size:14,
+                    },
                 },
-            }]
+            }
 		},
-		tooltips: {
-			mode: 'nearest',
-			intersect: false
-		},
-		hover: {
-			mode: 'nearest',
-			intersect: false
-		}
 	}
 };
 
